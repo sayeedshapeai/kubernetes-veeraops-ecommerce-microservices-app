3e312e826b37fd95c6295dfb42fb17d1bd518f5a
Sayeed Uddin Shaik <sayeedmohd1704@gmail.com>
2026-09-19 00:51:53 -0400

    Change EKS instance type from t2.medium to c7i-flex.large

--- EKS-Terraform/main.tf
+++ EKS-Terraform/main.tf
@@ -297,7 +297,7 @@ resource "aws_eks_node_group" "node_group" {
 
 resource "aws_instance" "eks" {
     ami           = "ami-02dfbd4ff395f2a1b"
-    instance_type = "t2.medium"
+    instance_type = "c7i-flex.large"
     subnet_id     = aws_subnet.public1.id
     vpc_security_group_ids = [aws_security_group.allow_all.id]
     root_block_device {
