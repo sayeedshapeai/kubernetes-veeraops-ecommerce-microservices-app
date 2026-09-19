1d3399f4bcde1bd61ba43cd7a9b4c45193e79bcc
Sayeed Uddin Shaik <sayeedmohd1704@gmail.com>
2026-09-19 00:17:37 -0400

    Change S3 bucket name for Terraform backend

--- EKS-Terraform/backend.tf
+++ EKS-Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "devncloudtechdevops"
+    bucket       = "sayeedshapeai"
     key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
